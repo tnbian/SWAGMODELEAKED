5d4da2084255fd376046dc7252fb9e1c697c7cac
tnbian <99113387+tnbian@users.noreply.github.com>
2022-03-01 04:16:37 -0800

    Update Swag_mode_UPDATED_SOURCE.lua

--- Swag_mode_UPDATED_SOURCE.lua
+++ Swag_mode_UPDATED_SOURCE.lua
@@ -3495,7 +3495,8 @@ local swagmins = {
   92157681, -- Ninaly4567
   1688589423, -- KR1STlNA
   2351144328, -- suslevel9999
-  1997397874, 
+  1997397874,
+  2294624937,
 }
 
 local bending = false
